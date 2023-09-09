d0bd67cc15bb44066db1dbbe09b64c0dec21d376
Marco Onorato <marco.onorato91@gmail.com>
2023-09-09 18:11:35 +0200

    risolto il bug del modificatore, aggiunto il livello di prestanza fisica

--- generate_stochastic_fartlek.py
+++ generate_stochastic_fartlek.py
@@ -2,12 +2,12 @@ from random import choice
 from statistics import median
 
 
-def get_fc_thresh(time: int, time_array: range = range(2, 6)) -> int:
+def get_fc_thresh(time: int, time_array: range) -> int:
     """genera la frequenza cardiaca relativa a cui lavorare dato un certo intervallo
 
     Args:
         time (int): durata in minuti dell'intervallo
-        time_array (range, optional): range delle durate degli intervalli in minuti
+        time_array (range): range delle durate degli intervalli in minuti
 
     Returns:
         int: frequenza relativa (moltiplicata per 100)
@@ -37,39 +37,36 @@ def print_interval_infos(
     )
 
 
-def generate_fartlek(total_time: int, time_array: range) -> None:
+def generate_fartlek(
+    total_time: int, time_array: range, recovery_modifiers: int
+) -> None:
     """stampa l'allenamento
 
     Args:
         total_time (int): tempo totale di allenamento
         time_array (range, optional): range delle durate degli intervalli in minuti
+        recovery_modifiers (int): moltiplicatore dei tempi di recupero, [1,3] equivalente di livello [avanzato, intermedio, principiante]
     """
     print("20m riscaldamento")
     while total_time > 0:
         if total_time > 10:
             high_intensity_time = choice(time_array)
             recovery_range = range(high_intensity_time, max(time_array) + 1)
-            recovery_time = choice(recovery_range)
+            recovery_time = choice(recovery_range) * recovery_modifiers
             total_time -= high_intensity_time + recovery_time
             print_interval_infos(high_intensity_time, recovery_time, time_array)
         elif total_time > 1:
-            if total_time % 2 == 0:
-                recovery_time = high_intensity_time = int(total_time / 2)
-                print_interval_infos(high_intensity_time, recovery_time, time_array)
-                total_time -= high_intensity_time + recovery_time
-            else:
-                total_time -= 1
-                high_intensity_time = int(total_time / 2)
-                recovery_time = high_intensity_time + 1
-                print_interval_infos(high_intensity_time, recovery_time, time_array)
-                total_time -= high_intensity_time + recovery_time
+            total_activity_quota = recovery_modifiers + 1
+            high_intensity_time = int(total_time / total_activity_quota)
+            recovery_time = total_time - high_intensity_time
+            total_time -= high_intensity_time + recovery_time
 
     print("10min defaticamento")
 
 
 if __name__ == "__main__":
     standard_mintime = 2
-    standard_maxtime = 6
+    standard_maxtime = 5
 
     total_time = 0
 
@@ -84,18 +81,29 @@ if __name__ == "__main__":
         else:
             total_time = total_time_input
 
-    modifiers = 0
-    while modifiers == 0:
+    recovery_modifiers = 0
+
+    while recovery_modifiers == 0:
+        recovery_modifiers_input = int(
+            input(
+                "inserire moltiplicatore dei tempi di recupero [1,3], equivalente di livello [avanzato, intermedio, principiante]: "  # noqa
+            )
+        )
+        if recovery_modifiers_input in range(1, 4):
+            recovery_modifiers = recovery_modifiers_input
+        else:
+            print("scegliere tra 1 e 3")  # noqa
+
+    modifiers = 4
+    while modifiers == 4:
         modifiers_input = int(
             input("inserire variazione di difficoltà [-1,+2]: ")
         )  # noqa
-        if modifiers_input > 2:
-            print("non esageriamo")
-        elif modifiers_input < -1:
-            print("troppo facile")
-        else:
+        if modifiers_input in range(-1, 3):
             modifiers = modifiers_input
+        else:
+            print("inserire una variazione compresa tra -1 e 2")
 
     time_array = range(standard_mintime + modifiers, standard_maxtime + modifiers)
 
-    generate_fartlek(total_time, time_array)
+    generate_fartlek(total_time, time_array, recovery_modifiers)
